e85fd5a66910366f121ed82ba361d6e40e5bf4c3
Sonu Kushwaha <imsonukushwaha@gmail.com>
2025-05-09 21:45:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <h1 className="text-8xl text-amber-600">sonu</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
