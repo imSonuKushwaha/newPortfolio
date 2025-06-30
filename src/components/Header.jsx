7272f4c7ba0508a26157d033ae98c3681e774dc6
Sonu Kushwaha <imsonukushwaha@gmail.com>
2025-06-30 23:15:28 +0530

    drive fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -69,7 +69,7 @@ const Header = () => {
           </li>
           <li>
             <a
-              href="https://drive.google.com/file/d/1ovQCRkkXZ8VExiv8pbCOcBryzEwInTPi/view?usp=sharing"
+              href="https://drive.google.com/file/d/1wYFQ_Az8GaKErkdNhbYE8KgZN9h9jfxc/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               onClick={() => setIsMenuOpen(false)}
