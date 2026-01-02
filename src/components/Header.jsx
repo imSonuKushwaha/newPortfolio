1ee0726842db10566bc3cdc8549d74780daea0a0
Sonu Kushwaha <imsonukushwaha@gmail.com>
2026-01-02 15:00:03 +0530

    new link for resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -69,7 +69,7 @@ const Header = () => {
           </li>
           <li>
             <a
-              href="https://drive.google.com/file/d/1wYFQ_Az8GaKErkdNhbYE8KgZN9h9jfxc/view?usp=sharing"
+              href="https://drive.google.com/file/d/11lWX4ELR2-d9rZvdWHJPv9WNfszi9zdN/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               onClick={() => setIsMenuOpen(false)}
